d581d2af6e5f77275b9906b79274a48160a0b217
Hasan Mavlonov <hmavlanov79@gmail.com>
2024-10-30 17:52:45 +0500

    Added DebtModel

--- debts/models.py
+++ debts/models.py
@@ -14,3 +14,19 @@ class UserModel(AbstractUser):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
+
+class DebtModel(models.Model):
+    giver = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    receiver_phone_number = models.CharField(max_length=11, unique=True)
+    giver_phone_number = models.CharField(max_length=11, unique=True)
+    amount = models.IntegerField()
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'from {self.giver.username} to {self.receiver.username}'
+
+    class Meta:
+        db_table = 'debts'
+        verbose_name = 'Debt'
+        verbose_name_plural = 'Debts'
