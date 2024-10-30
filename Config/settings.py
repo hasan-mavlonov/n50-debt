5cfeeca54b34ffa1559a47ae01f930bea84c4e19
Hasan Mavlonov <hmavlanov79@gmail.com>
2024-10-30 17:52:00 +0500

    added UserModel from AbstractUser adding phone_number

--- Config/settings.py
+++ Config/settings.py
@@ -123,3 +123,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+        'rest_framework_csv.renderers.CSVRenderer',
+        'rest_framework_yaml.renderers.YAMLRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- debts/models.py
+++ debts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+
+class UserModel(AbstractUser):
+    phone_number = models.CharField(max_length=11, unique=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
