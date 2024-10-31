69a1c006c19f1d4b528352460e1c9ad37ff67675
Hasan Mavlonov <hmavlanov79@gmail.com>
2024-10-31 23:48:34 +0500

    created user's views

--- debts/views/users.py
+++ debts/views/users.py
@@ -1,1 +1,42 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from debts.models import UserModel
+from debts.serializers import UserSerializer
+
+
+@api_view(['GET', 'POST'])
+def user_list_create(request):
+    if request.method == 'GET':
+        users = UserModel.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def user_detail(request, pk):
+    user = get_object_or_404(UserModel, pk=pk)
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.validated_data)
+    elif request.method == 'PUT':
+        serializer = UserSerializer(instance=user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+    elif request.method == 'PATCH':
+        serializer = UserSerializer(instance=user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
