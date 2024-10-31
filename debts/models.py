746ae5f0b9a40f16d6340ccce3d5a166e9d7d805
Hasan Mavlonov <hmavlanov79@gmail.com>
2024-10-31 23:15:26 +0500

    UserModel and DebtModel update

--- debts/models.py
+++ debts/models.py
@@ -2,9 +2,23 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class UserModel(AbstractUser):
     phone_number = models.CharField(max_length=11, unique=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name="custom_user_groups",  # Custom related name to avoid conflicts
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name="custom_user_permissions",  # Custom related name to avoid conflicts
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
 
     def __str__(self):
         return self.username
@@ -16,10 +30,14 @@ class UserModel(AbstractUser):
 
 
 class DebtModel(models.Model):
-    giver = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    receiver = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    receiver_phone_number = models.CharField(max_length=11, unique=True)
-    giver_phone_number = models.CharField(max_length=11, unique=True)
+    giver = models.ForeignKey(
+        UserModel, on_delete=models.CASCADE, related_name="debts_given"
+    )
+    receiver = models.ForeignKey(
+        UserModel, on_delete=models.CASCADE, related_name="debts_received"
+    )
+    receiver_phone_number = models.CharField(max_length=11)
+    giver_phone_number = models.CharField(max_length=11)
     amount = models.IntegerField()
     status = models.BooleanField(default=True)
 
